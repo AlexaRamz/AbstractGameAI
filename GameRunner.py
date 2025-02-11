008fa96a59f9de93eea0701df98e80a1119a69b8
JasonTurner <jasonturner119@icloud.com>
2025-02-11 13:49:30 -0800

    Adds a GameRunner class to facilitate two models playing a game. Adds a main.py that constructs a GameRunner and calls play.

---
+++ GameRunner.py
@@ -0,0 +1,24 @@
+
+from Games.Game import Game, Player
+from Models.Model import Model
+
+class GameRunner:
+
+    def __init__(self, game: Game, player_one: Model, player_two: Model):
+        # Creates a GameRunner where the two models play the given game
+        self.game = game
+        self.player_one = player_one
+        self.player_two = player_two
+        self.player_one.set_game_and_player(game, Player.FIRST)
+        self.player_two.set_game_and_player(game, Player.SECOND)
+
+    def play(self) -> Player:
+        # Play as many turns of the game as are required to determine a winner
+        # The winning player is returned
+        while self.game.get_winner() is None:
+            current = self.game.get_current_player()
+            if current == Player.FIRST:
+                self.player_one.take_move()
+            else:
+                self.player_two.take_move()
+        return self.game.get_winner()
